d53d485377caf46d266a7d00c5142728ed79abef
tarunagarwal27@gmail.com <tarun.agarwal@holachef.com>
2017-11-21 08:37:20 +0530

    added routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,16 +10,9 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   constructor(private heroService: HeroService) { }
